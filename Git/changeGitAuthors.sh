86f97ab6232009ded055cf4d9eb60fe3fce480ae
Josh Buhler <josh@buhler.me>
2020-05-26 10:42:11 -0600

    Git script update

---
+++ Git/changeGitAuthors.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Because I'm always committing using the wrong account info...
+git filter-branch --env-filter '
+
+OLD_EMAIL="wrong@email"
+CORRECT_NAME="correctname"
+CORRECT_EMAIL="correct@email"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
